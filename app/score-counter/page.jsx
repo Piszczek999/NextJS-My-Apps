8f85ea1fa89166d0cdfb3054ed4d91744a1f43dc
Piszczek <ognisty001@gmail.com>
2023-08-27 11:19:30 +0200

    ScoreCard functionality added, cleaned up globals.css

--- app/score-counter/page.jsx
+++ app/score-counter/page.jsx
@@ -3,16 +3,10 @@ import ScoreCard from "../components/ScoreCard";
 export default function ScoreCounter() {
   return (
     <div>
-      <h2>Score Counter Page</h2>
-      <div className="flex items-center">
-        <ScoreCard
-          title={{ value: "Blue Team", color: "text-blue-500" }}
-          score={1}
-        />
-        <ScoreCard
-          title={{ value: "Red Team", color: "text-red-500" }}
-          score={0}
-        />
+      <h2 className="text-center">Score Counter Page</h2>
+      <div className="flex justify-center">
+        <ScoreCard title={{ value: "Blue Team", color: "text-blue-500" }} />
+        <ScoreCard title={{ value: "Red Team", color: "text-red-500" }} />
       </div>
     </div>
   );
